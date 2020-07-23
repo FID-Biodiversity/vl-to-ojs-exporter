929fcec218cfbd34032e7f41e83429d3b9f716e3
Adrian Pachzelt <a.pachzelt@gmail.com>
2020-07-23 10:24:31 +0200

    Included first running test

--- ojs/xmlgenerator.py
+++ ojs/xmlgenerator.py
@@ -1,5 +1,7 @@
+from VisualLibrary import Journal, Volume, Issue, Article, VisualLibraryExportElement
 from abc import ABC, abstractmethod
-from VisualLibrary.VisualLibrary import Journal, Volume, Issue, Article, VisualLibraryExportElement
+from bs4 import BeautifulSoup as Soup
+from datetime import datetime
 from jinja2 import Environment, FileSystemLoader
 
 from templates.global_functions import register_global_values_to_environment
@@ -33,6 +35,7 @@ class XmlGenerator(ABC):
             :returns: An XML string of the object.
             :rtype: str
         """
+
         template = self.template_environment.get_template(self.template_file_name)
         return template.render(self.template_configuration)
 
@@ -48,6 +51,8 @@ class OjsArticle(XmlGenerator):
     def __init__(self, vl_article: Article, template_configuration):
         super().__init__(template_configuration)
 
+        assert isinstance(vl_article, Article)
+
         self.abstract = None
         self.authors = vl_article.authors
         self.doi = vl_article.doi
@@ -57,12 +62,14 @@ class OjsArticle(XmlGenerator):
         self.prefix = self._get_title_prefix(vl_article.title)
         self.submission_files = vl_article.files
         self.title = vl_article.title
+        self.language = self._get_primary_language(vl_article.languages)
+        self.submission_date = self._get_submission_date_from_files(vl_article.files)
 
         # Variables expected in the template
         self.add_variable_to_template_configuration(self.ARTICLES_STRING, [self])
 
     @property
-    def template_file_name(self):
+    def template_file_name(self) -> str:
         return self.ARTICLES_TEMPLATE_FILE_NAME
 
     def _get_title_prefix(self, article_title: str) -> (str, None):
@@ -74,37 +81,74 @@ class OjsArticle(XmlGenerator):
         else:
             return None
 
+    def _get_primary_language(self, languages) -> (str, None):
+        if languages:
+            if isinstance(languages, list):
+                return languages[0]
+            elif isinstance(languages, set):
+                return languages.pop()
+
+        return None
+
+    def _get_submission_date_from_files(self, submission_files: list) -> str:
+        for submission_file in submission_files:
+            if submission_file.date_uploaded is not None:
+                return submission_file.date_uploaded
+
+        return datetime.today().date().isoformat()
+
 
 class OjsIssue(XmlGenerator):
 
-    ISSUES_TEMPLATE_FILE_NAME = 'issues.xml'
+    ISSUE_STRING = Issue.ISSUE_STRING
     ISSUES_STRING = 'issues'
+    ISSUES_TEMPLATE_FILE_NAME = 'issues.xml'
+    MODS_TAG_DETAIL_STRING = VisualLibraryExportElement.MODS_TAG_DETAIL_STRING
+    MODS_TAG_NUMBER_STRING = VisualLibraryExportElement.MODS_TAG_NUMBER_STRING
+    MODS_TAG_PART_STRING = VisualLibraryExportElement.MODS_TAG_PART_STRING
+    TYPE_STRING = VisualLibraryExportElement.TYPE_STRING
+    VOLUME_STRING = Volume.VOLUME_STRING
 
     def __init__(self, vl_issue: Issue, template_configuration):
         super().__init__(template_configuration)
 
+        assert isinstance(vl_issue, Issue)
+
+        # This is a shortcut! Resolving a parent would take longer!
+        volume_number = self._get_volume_number(vl_issue)
+
         self.articles = [OjsArticle(article, template_configuration) for article in vl_issue.articles]
-        self.volume_number = None
-        if vl_issue.parent is not None:
-            self.volume_number = vl_issue.parent.label
-        self.issue_number = vl_issue.label
-        self.publication_year = vl_issue.publication_date.year
+        self.volume_number = volume_number if volume_number is not None else vl_issue.parent.number
+        self.issue_number = vl_issue.number
+        self.publication_year = vl_issue.publication_date
         self.is_current_issue = False
         self.id = vl_issue.id
-        self.date_published = vl_issue.publication_date
-        self.date_modified = vl_issue.publication_date
+        self.date_published = datetime.strptime(vl_issue.publication_date, '%Y').date().isoformat()
+        self.date_modified = datetime.strptime(vl_issue.publication_date, '%Y').date().isoformat()
 
         self.add_variable_to_template_configuration(self.ISSUES_STRING, [self])
 
     @property
-    def template_file_name(self):
+    def template_file_name(self) -> str:
         return self.ISSUES_TEMPLATE_FILE_NAME
+
+    def _get_volume_number(self, vl_issue: Issue) \
+            -> (str, None):
+        try:
+            info_node = vl_issue.metadata.find(self.MODS_TAG_PART_STRING).find(
+                self.MODS_TAG_DETAIL_STRING, {self.TYPE_STRING: self.VOLUME_STRING})
+
+            return info_node.find(self.MODS_TAG_NUMBER_STRING).text
+        except AttributeError:
+            return None
 
 
 class OjsVolume(XmlGenerator):
 
     def __init__(self, vl_volume: Volume, template_configuration):
         super().__init__(template_configuration)
+
+        assert isinstance(vl_volume, Volume)
 
         self.issues = [OjsIssue(issue, template_configuration) for issue in vl_volume.issues]
         self.articles = [OjsArticle(article, template_configuration) for article in vl_volume.articles]
--- tests/testXmlGeneration.py
+++ tests/testXmlGeneration.py
@@ -1,17 +1,46 @@
+import os
+
 from configuration.Configurator import Configurator
-from VisualLibary import VisualLibrary
-from ojs.xmlgenerator import OjsXmlGenerator
+from VisualLibrary import VisualLibrary
+from ojs.xmlgenerator import OjsXmlGenerator, OjsIssue
+
+this_files_directory = os.path.dirname(os.path.realpath(__file__))
+TEST_DATA_DIRECTORY = '{base_dir}/data'.format(base_dir=this_files_directory)
+
+
+class MockConfigurator(Configurator):
+    def __init__(self):
+        super().__init__()
+
+    def get_template_configuration(self):
+        return {
+            'languages': ['de_DE'],
+            'user_group_reference_label': 'Autor*in',
+            'article_text_genre_label': 'Artikeltext',
+            'file_uploading_ojs_user': 'ojs_admin',
+            'article_reference_label': 'ART'
+        }
 
 
 class TestXmlGeneration:
     def test_issue_xml_generation(self):
-        configurator = Configurator()
+        xml_test_file = '{base_dir}/generator-test-issue.xml'.format(base_dir=TEST_DATA_DIRECTORY)
+
+        configurator = MockConfigurator()
         vl = VisualLibrary()
+        vl_issue = vl.get_element_from_xml_file(xml_test_file)
 
-        item_id = '10802368'
-        vl_item = vl.get_element_for_id(item_id)
+        ojs_xml_generator = OjsXmlGenerator(configurator)
+        ojs_issue = ojs_xml_generator.convert_vl_objecto_to_ojs_object(vl_issue)
 
-        ojs_xml_generator = OjsXmlGenerator()
-        issue_xml = ojs_xml_generator.get_xml_from_vl_object(vl_item)
+        assert isinstance(ojs_issue, OjsIssue)
+        assert len(ojs_issue.articles) == 10
+        assert ojs_issue.volume_number == '101 AB'
+        assert ojs_issue.issue_number == '1. Lieferung'
+        assert ojs_issue.publication_year == '1942'
+        assert ojs_issue.date_published == '1942-01-01'
+        assert ojs_issue.date_modified == '1942-01-01'
+        assert ojs_issue.id == '10802368'
+        assert not ojs_issue.is_current_issue
 
 
